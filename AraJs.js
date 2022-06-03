024452bc6ce316318153ad71e85fdf2b928a572e
Ara-marketing <67396737+Ara-marketing@users.noreply.github.com>
2022-06-03 15:10:55 +1200

    V1.0.0

--- AraJs.js
+++ AraJs.js
@@ -1,11 +1,9 @@
 /***Ara Js library***/
-var AraLib = {
+var Ara = {
     version: '1.0.0',
-    printVersion: function print() {
-        console.log('version' + this.version);
+    copyright: 'Ara webteam',
+    printVersion: () => {
+        alert(Ara.version);
     }
-}
-function Ara(){
-    this.version = '2.0.0';
 }
 console.log('not working?');
